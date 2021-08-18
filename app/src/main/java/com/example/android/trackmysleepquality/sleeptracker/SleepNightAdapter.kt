cc8316a2d247befeba490a5b97db72a922cd01a1
Mostafa Mohammed <mostafa.mohammed.sayed.awad@gmail.com>
2021-08-18 03:27:28 +0200

    1-replaced scrollView with RecyclerView(actually thathis was done last commit but i forgot to put its message).2-Created SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>.3-Created a list of SleepNight which is the data to be displayed by the RV.4-Override functions onCreateViewHolder(),OnBindViewHolder(),getItemCount() and commented the rple of each one.5-declared an instance of SleepNightAdapter in SleepTrackerFragment.6-Set the RV.adapter to the instance of SleepNightAdapter.7-Observed nights in the viewModel and set its value to our adapter data list.--this makes the app display a list of integers representing quality of each stored night

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,4 +16,43 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
 
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+
+    //the list o be displayed
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            //this adapter method notify RV when the list change and make RV re-draw the whole list
+            notifyDataSetChanged()
+        }
+    /*onCreateViewHolder(): creates the viewHolder that the RV will deal with since RV does not deal
+    * with data, the arguments: parent which is the RV itself, ViewType is an Int that will be used if more than 1
+    * viewHolder is used, TextItemViewHolder is a pre-made viewHolder which u can find in Util.kt file,
+    * this fun returns a viewHolder that takes a view as an argument. This function is called when the app starts
+    * at first time and when the views displayed on the screen increase */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+
+    /*onBindViewHolder(): is called to bind an item at a specified position in the list to the
+    * viewHolder. This fun also RECYCLES the views as it uses viewholders of items that are no longer on the screen
+    * and reset its values with the values of the item that will be displayed on the screen  */
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    //the RV must know how many items will it draw, this fun provide items count
+    override fun getItemCount()= data.size
+
+}
