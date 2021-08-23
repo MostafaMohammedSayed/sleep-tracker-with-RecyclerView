7db9a7dc1bee2e176401ae76439d534c5008ed10
Mostafa Mohammed <mostafa.mohammed.sayed.awad@gmail.com>
2021-08-23 02:52:57 +0200

    added a variable in the list item xml to bind it with the clickListener class

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,9 +21,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
@@ -44,7 +41,7 @@ class SleepNightAdapter: ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(Sl
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
         //set the values of viewHolder properties to the items values
-        holder.bind(item)
+        holder.bind(item, clickListener)
 
     }
 
@@ -53,7 +50,8 @@ class SleepNightAdapter: ListAdapter<SleepNight,SleepNightAdapter.ViewHolder>(Sl
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding): RecyclerView.ViewHolder(binding.root){
 
         fun bind(
-            item: SleepNight
+            item: SleepNight,
+            clickListener: SleepNightListener
         ) {
             binding.sleep = item
             binding.executePendingBindings()//slightly faster to bind the views
